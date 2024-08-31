2f4347a0d31f7748b50cebdfbac18fc53908ca8d
Volker Dsea <git.dsea@tutanota.com>
2024-08-31 20:21:35 +0200

    edit keymap.c

--- keyboards/bastardkb/charybdis/4x6/keymaps/my-keymap/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/my-keymap/keymap.c
@@ -58,31 +58,31 @@ static uint16_t auto_pointer_layer_timer = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
+        KC_ESC,    KC_1, DRGSCRL,RCS(KC_TAB),LCTL(KC_TAB),KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        KC_ESC,    KC_W,    KC_L,    KC_R,    KC_B,    KC_Z,   KC_SCLN ,    KC_Q,    KC_U,    KC_D,    KC_J, KC_MINS,
+        KC_ESC,    KC_W,    KC_L,    KC_R,    KC_B,    KC_Z,   KC_SCLN ,    KC_Q,    KC_U,    KC_D,    KC_J, DRGSCRL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
 KC_LSFT,C_S_T(KC_S),GUI_T(KC_H),LALT_T(KC_N),LCTL_T(KC_T), KC_COMM,    KC_DOT ,LCTL_T(KC_A),LALT_T(KC_E),GUI_T(KC_O), C_S_T(KC_I), KC_LSFT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_TAB ,    KC_F,    KC_M,    KC_V,    KC_C,KC_SLSH ,       KC_G,    KC_P, KC_X   ,  KC_K  , KC_Y   , KC_ENTER,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                    KC_LEFT, LT(LAYER_LOWER,KC_SPC),KC_RGHT,     KC_UP ,  MO(LAYER_LOWER),
+                                    KC_LEFT, KC_SPC,KC_RGHT,     KC_UP ,  MO(LAYER_LOWER),
                                             KC_BSPC,KC_BTN1,     KC_DOWN
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
   [LAYER_LOWER] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+       KC_TILD, KC_EXLM,   KC_AT, KC_QUOT, KC_LBRC, KC_RBRC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       RGB_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LBRC,   KC_P7,   KC_P8,   KC_P9, KC_RBRC, XXXXXXX,
+       RGB_MOD, KC_UNDS, KC_MINS, KC_DQUO, KC_LPRN, KC_RPRN,    KC_CIRC, KC_PERC, KC_ASTR, KC_AMPR, KC_HASH, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
 KC_LSFT,C_S_T(KC_0),GUI_T(KC_1),LALT_T(KC_2),LCTL_T(KC_3), KC_COMM,    KC_DOT ,LCTL_T(KC_4),LALT_T(KC_5),GUI_T(KC_6), C_S_T(KC_7), KC_LSFT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-      RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_PAST,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_PDOT,
+       XXXXXXX, KC_TILD, KC_PIPE, KC_EQL, KC_LCBR, KC_RCBR,      KC_AT, KC_EXLM, KC_PLUS, KC_BSLS,  KC_DLR, KC_PDOT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  XXXXXXX, XXXXXXX, _______,       KC_8,    KC_9,
-                                           XXXXXXX, XXXXXXX,       KC_P0
+                             RCS(KC_TAB),  KC_SPC, LCTL(KC_TAB),      KC_8,    KC_9,
+                                           KC_BSPC,KC_BTN2,        KC_P0
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
