46d71ceb38ca981a09d450729f3d95cb7ed25ccd
minimop <minimop@fedora.fritz.box>
2024-11-20 02:43:15 +0100

    update

--- keyboards/bastardkb/charybdis/4x6/keymaps/my-keymap/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/my-keymap/keymap.c
@@ -71,7 +71,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
           // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-             KC_DELETE,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,     KC_F18,  KC_F19,  KC_F20,  KC_A, KC_BTN1, KC_BTN2,
+             KC_DELETE,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,     KC_APP,  KC_CAPS,  KC_PSCR,  KC_A, KC_BTN1, KC_BTN2,
           // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
                KC_MINS,    KC_W,    KC_L,    KC_R,    KC_B,    KC_Z,    KC_SCLN,    KC_Q,    KC_U,    KC_D,    KC_J, DRGSCRL,
           // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
@@ -95,23 +95,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
                XXXXXXX, KC_CAPS , KC_PIPE, KC_DQUO, KC_PERC, XXXXXXX,      KC_AT, KC_EXLM, KC_ASTR, KC_BSLS,  KC_DLR, KC_TILD,
           // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                    RCS(KC_TAB),KC_SPC,LCTL(KC_TAB),    XXXXXXX, XXXXXXX,
+                                    RCS(KC_TAB),KC_SPC,LCTL(KC_TAB),    DF(LAYER_RAISE), XXXXXXX,
                                                     KC_BTN2,KC_BTN3,    KC_BTN2
           //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
   [LAYER_RAISE] = LAYOUT(
           // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-                KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+               KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
           // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-               KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
+                 KC_T,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
           // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-               KC_MPLY, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MUTE,
+                 KC_G, KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
           // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-               KC_MPRV, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
+                 KC_B, KC_LCTL,    PT_Z,    KC_X,    KC_C,    KC_V,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_LALT,
           // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                          _______, _______, XXXXXXX,    _______, XXXXXXX,
-                                                   _______, _______,    XXXXXXX
+                                          KC_LGUI, KC_SPC,   LOWER,    DF(LAYER_BASE),  KC_ENT,
+                                                  KC_LALT, KC_BSPC,     KC_DEL
           //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
